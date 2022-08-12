cd9b5e270299e7e4514731b0901a4a50c514a811
globz <globz@kittybomber.com>
2022-08-11 22:11:18 -0400

    Fixed screens + completed mobile layout + desktop

--- apps/primordial_web/assets/tailwind.config.js
+++ apps/primordial_web/assets/tailwind.config.js
@@ -7,10 +7,11 @@ module.exports = {
   ],
   theme: {
     screens: {
-      sm: '480px',
-      md: '768px',
-      lg: '976px',
-      xl: '1440px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
     },
     fontFamily: {
       sans: ['monospace', 'sans-serif'],
