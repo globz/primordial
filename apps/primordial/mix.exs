985808fb56d4b72aec28939cd7fb7017bd4615a8
globz <globz@kittybomber.com>
2023-01-02 12:16:33 -0500

    Added erlport for Python support

--- apps/primordial/mix.exs
+++ apps/primordial/mix.exs
@@ -40,7 +40,8 @@ defmodule Primordial.MixProject do
       {:ecto_sql, "~> 3.6"},
       {:postgrex, ">= 0.0.0"},
       {:jason, "~> 1.2"},
-      {:swoosh, "~> 1.3"}
+      {:swoosh, "~> 1.3"},
+      {:erlport, "~> 0.10.1"}
     ]
   end
 
