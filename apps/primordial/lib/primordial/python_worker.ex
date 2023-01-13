02e86f72b4402e6bd619776857369ccfc0e2c2a7
globz <globz@kittybomber.com>
2023-01-13 09:29:13 -0500

    Added :poolboy.checkout & checkin function

--- apps/primordial/lib/primordial/python_worker.ex
+++ apps/primordial/lib/primordial/python_worker.ex
@@ -58,43 +58,27 @@ defmodule Primordial.PythonWorker do
 
   Sends an asynchronous request to the :python instance of :my_module
 
-  Optional `dispatch` argument which can be used to dispatch a message
-  received by handle_info({:python, message}) to another Elixir module
+  Optional `lookup` argument which can be used to store a message
+  received by handle_info({:python, message})
 
   Return `:ok`
 
-  ## Example without dispatch
+  ## Example without lookup
 
    PythonWorker.cast(:my_module, message) :: :ok
 
-  ## Example with dispatch
+  ## Example with lookup
   
-   dispatch = %{module: MyModule, fun: :my_fun, args: [arg1, arg2]}
-
-   PythonWorker.cast(:my_module, message, dispatch) :: :ok
-
-  ## Example with dispatch & :message argument
-
-  dispatch = %{module: MyModule, fun: :my_fun, args: [`:message`, arg2]}
-
-  PythonWorker.cast(:my_module, message, dispatch) :: :ok
-
-  This will effectively replace `:message` with the actual message received
-  by handle_info({:python, message}) and will dispatched it to the assigned
-  dispatch module, along with any other arguments.
-
-  The dispatch result is automatically added to the current :python_worker
-  state and can be accessed via PythonWorker.lookup(:result)
+   PythonWorker.cast(:my_module, message, :result) :: :ok
   
   """
-  #@spec cast(module :: atom(), message :: term()) :: :ok
-  #@spec cast(module :: atom(), message :: term(), dispatch ::term()) :: :ok
+  @spec cast(module :: atom(), message :: term()) :: :ok
+  @spec cast(module :: atom(), message :: term(), lookup :: atom()) :: :ok
   def cast(module, message, lookup \\ nil) do
     Task.async(fn ->
       :poolboy.transaction(
         :python_worker,
         fn pid ->
-          IO.inspect(pid)
           try do
             GenServer.cast(pid, {:cast, %{module: module, message: message, lookup: lookup}})
           catch
@@ -128,9 +112,6 @@ defmodule Primordial.PythonWorker do
         lookup: nil,
         pid: pid
       }
-      # :poolboy only use one worker when multiple calls are made, should state
-      # be a list?
-      # http://www.davekuhlman.org/elixir-poolboy-erlport-datasci.html
       {:ok, state}
     end
   end
@@ -145,22 +126,24 @@ defmodule Primordial.PythonWorker do
 
   @impl true
   def handle_cast({:cast, params}, %{pid: pid} = state) do
-    IO.inspect(pid)
     %{module: module, message: message, lookup: lookup} = params
     :python.call(pid, module, :register_handler, [self()])
     :python.cast(pid, message)
     Logger.info("[#{__MODULE__}] Handled cast #{inspect pid}")
-    :poolboy.checkout(:python_worker, pid)
+
+    # Check out current worker if cast is expecting an async lookup
+    lookup_checkout(lookup, pid)
+    
     {:noreply, %{state | lookup: lookup}}
   end
 
   @impl true  
   def handle_info({:python, message}, %{pid: pid, lookup: lookup} = state) do
     Logger.info("[#{__MODULE__}] Handled :python info #{inspect pid}")
-    IO.inspect(lookup)
-    # Lookup key detected
-    # Store the result into the AsyncRegistry
-    if !is_nil(lookup), do: AsyncRegistry.set(lookup, message)
+
+    # Store message into the AsyncRegistry under the lookup key
+    # check the worker back into the process pool
+    lookup_checkin(lookup, message, pid)
 
     {:noreply, %{state | lookup: nil}, :hibernate}
   end
@@ -170,13 +153,13 @@ defmodule Primordial.PythonWorker do
     Logger.info("[#{__MODULE__}] Handled terminate")
     :ok
   end
+
+  defp lookup_checkout(lookup, pid) do
+    if !is_nil(lookup), do: :poolboy.checkout(:python_worker, pid)
+  end
+
+  defp lookup_checkin(lookup, message, pid) do
+    if !is_nil(lookup), do: (AsyncRegistry.set(lookup, message);
+      :poolboy.checkin(:python_worker, pid))
+  end
 end
-
-# TODO
-
-# Investigate why poolboy only use one worker
-# Fix lookup key being overwritten, should track it with make_ref and python
-# message should send back the ref along with the result
-# or checkout workers on cast and re-enable them when a message is
-# received....however if no response is expected then do not checkout the
-# worker, then simply check back the worker once the response is received!
