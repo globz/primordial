f9cd9890344e02add348863e03ff8c2e0f6eee1f
globz <globz@kittybomber.com>
2023-11-17 14:22:52 -0500

    Updated moduledoc

--- apps/primordial/lib/mix/tasks/psql.start.ex
+++ apps/primordial/lib/mix/tasks/psql.start.ex
@@ -1,5 +1,7 @@
 defmodule Mix.Tasks.Psql.Start do
-  @moduledoc "Start psql instance"
+  @moduledoc """
+  Start psql instance - Invocation: `mix psql.start & wait $!`
+  """
   @shortdoc "Start psql instance"
 
   use Mix.Task
