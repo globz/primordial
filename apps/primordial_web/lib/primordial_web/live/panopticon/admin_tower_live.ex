445ef4b95f0995aac98f3b408907b63761721cf9
globz <globz@kittybomber.com>
2023-11-19 20:14:21 -0500

    Completed first draft of drawer sections

--- apps/primordial_web/lib/primordial_web/live/panopticon/admin_tower_live.ex
+++ apps/primordial_web/lib/primordial_web/live/panopticon/admin_tower_live.ex
@@ -1,4 +1,5 @@
 defmodule PrimordialWeb.AdminTowerLive do
+  alias PrimordialWeb.Enroll.UserLive.Show
   use PrimordialWeb, :live_view
   alias Phoenix.LiveView.JS
 
@@ -93,15 +94,15 @@ defmodule PrimordialWeb.AdminTowerLive do
               aria-controls="dropdown-items"
             >
               <svg
-                class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
+                class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
                 aria-hidden="true"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="currentColor"
-                viewBox="0 0 18 21"
+                viewBox="0 0 20 18"
               >
-                <path d="M15 12a1 1 0 0 0 .962-.726l2-7A1 1 0 0 0 17 3H3.77L3.175.745A1 1 0 0 0 2.208 0H1a1 1 0 0 0 0 2h.438l.6 2.255v.019l2 7 .746 2.986A3 3 0 1 0 9 17a2.966 2.966 0 0 0-.184-1h2.368c-.118.32-.18.659-.184 1a3 3 0 1 0 3-3H6.78l-.5-2H15Z" />
+                <path d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z" />
               </svg>
-              <span class="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap ml-2">Test</span>
+              <span class="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap ml-2">Users</span>
               <svg
                 class="w-3 h-3"
                 aria-hidden="true"
@@ -124,7 +125,7 @@ defmodule PrimordialWeb.AdminTowerLive do
                   href="#"
                   class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
                 >
-                  Products
+                  New Admin
                 </a>
               </li>
               <li>
@@ -132,7 +133,7 @@ defmodule PrimordialWeb.AdminTowerLive do
                   href="#"
                   class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
                 >
-                  Billing
+                  Reset Admin Password
                 </a>
               </li>
               <li>
@@ -140,7 +141,15 @@ defmodule PrimordialWeb.AdminTowerLive do
                   href="#"
                   class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
                 >
-                  Invoice
+                  Show All Users
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+                >
+                  Edit Users
                 </a>
               </li>
             </ul>
@@ -183,23 +192,6 @@ defmodule PrimordialWeb.AdminTowerLive do
               <span class="inline-flex items-center justify-center w-3 h-3 p-3 ms-3 text-sm font-medium text-blue-800 bg-blue-100 rounded-full dark:bg-blue-900 dark:text-blue-300">
                 3
               </span>
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
-            >
-              <svg
-                class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                viewBox="0 0 20 18"
-              >
-                <path d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z" />
-              </svg>
-              <span class="flex-1 ms-3 whitespace-nowrap ml-2">Users</span>
             </a>
           </li>
           <li>
